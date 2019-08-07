d576d4c2a02c5cf1ef82ba88cbc54f8930baec63
BenB-C and JHorgen <ben+josh@>
2019-08-07 16:57:11 -0700

    add resources for many-to-many relationship

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :albums do
     resources :songs
   end
-  resources :artists
+  resources :artists do
+    resources :albums
+  end
+  resources :albums do
+    resources :artists
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
